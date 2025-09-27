c1ddc5f361a75dc01ae0de323f36e6c101dad265
Conor Callaghan <157692610+Conor153@users.noreply.github.com>
2025-09-27 09:26:41 +0100

    Initialize Vite React TypeScript app
    Add initial project files for a Vite-based React application using TypeScript, including configuration, source files, and assets.

---
+++ vite/react-App/src/SignIn.tsx
@@ -0,0 +1,15 @@
+function SignIn(){
+  return <div>
+    <h1>Sign In</h1>
+    <label>Username: </label>
+    <input type="text" />
+    <br></br>
+    <label>Password: </label>
+    <input type="password" />
+    <br></br>
+    <button>Sign In</button>
+  </div>
+  ;
+}
+
+export default SignIn;
